6bd99b2f41d3233b9c9897617d549c8f9f37cd8e
backfirecs <361046899@qq.com>
2018-07-06 08:55:03 +0800

    add account activations

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,8 +20,10 @@ $factory->define(App\Models\User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'activated' => true,
         'remember_token' => str_random(10),
         'created_at' => $date_time,
         'updated_at' => $date_time,
+
     ];
 });
